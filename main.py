4c9c94db847f6e95f4a98d1b012371300cea5a8c
Nishtha-Parashar <nishthaparashar01@gmail.com>
2023-06-24 17:54:09 +0530

    new changes

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = 'static'
+ALLOWED_EXTENSIONS = {'png', 'webp', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def processImage():
+    pass 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/About")
+def about():
+    return render_template("About.html")
+
+@app.route("/edit", methods = ["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        if 'file' not in request.files:
+            flash('No file part')
+            return "ERROR!"
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No file selected')
+            return "No file Selected"
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template("index.html")
 
 app.run(debug=True, port=5001)
